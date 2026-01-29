472ca805d07b6c463745b1e9d81057b578035d71
Nicholas Mack <nam7nam14@gmail.com>
2026-01-28 23:21:36 -0500

    Added files

--- src/components/DocumentFile.tsx
+++ src/components/DocumentFile.tsx
@@ -1,6 +1,6 @@
 import "./DocumentFile.css";
-import fileIcon from "/icons/svgs/file.svg";
-import "./DocumentFile.css";
+import fileIcon from "../assets/icons/file.svg";
+import delIcon from "../assets/icons/xmark.svg";
 
 interface DocumentFileProps {
     file: {
@@ -14,18 +14,21 @@ export default function DocumentFile({ file }: DocumentFileProps) {
     return (
         <section className="doc-file">
             <div className="file-content">
-                {/*<img src={fileIcon} alt="file-icon"/>*/}
-                <span>fgbfgdgtds</span>
-                <main className="file-info">
+                <img className="file-icon" src={fileIcon} alt="file-icon"/>
+                <span className="file-type">txt</span>
+
+                <div className="file-info">
                     <h3 className="file-name">{file.fileName}</h3>
                     <div className="file-meta">
                         <span className="file-size">{file.fileSize} KB</span>
                         <span className="file-chars">{file.fileChars} chars</span>
                     </div>
-                </main>
-                <button className="file-del">trash</button>
+                </div>
+
+                <button className="file-del" type="button" aria-label="Remove file">
+                    <img className="file-icon" src={delIcon} alt="delete-icon"/>
+                </button>
             </div>
         </section>
     );
 }
-
